551bfae549d63f6b23c28dcc7de3d290c82c03f5
Michelson <nsmichelson@gmail.com>
2019-12-23 13:58:32 -0800

    changed it so that when you are editing a venue, it placeholder fills the fields with data that already exists for that venue

--- app.py
+++ app.py
@@ -155,20 +155,20 @@ def show_venue(venue_id):
     data = {
     "id": venue_id,
     "name": venue.name,
-    "genres": venue.genres,
+    #"genres": venue.genres,
     "address": venue.address,
     "city": venue.city,
     "state": venue.state,
     "phone": venue.phone,
-    "website": venue.website,
+    #"website": venue.website,
     "facebook_link": venue.facebook_link,
-    "seeking_talent": venue.seeking_talent,
-    "seeking_description": venue.seeking_description,
-    "image_link": venue.image_link,
-    "past_shows": venue.past_shows,
-    "upcoming_shows": venue.upcoming_shows,
-    "past_shows_count": venue.past_shows_count,
-    "upcoming_shows_count": venue.upcoming_shows_count
+    #"seeking_talent": venue.seeking_talent,
+    #"seeking_description": venue.seeking_description,
+    #"image_link": venue.image_link,
+    #"past_shows": venue.past_shows,
+    #"upcoming_shows": venue.upcoming_shows,
+    #"past_shows_count": venue.past_shows_count,
+    #"upcoming_shows_count": venue.upcoming_shows_count
     }
     return render_template('pages/show_venue.html', venue=data)
   # shows the venue page with the given venue_id
@@ -305,18 +305,19 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
+  artistToEdit = Artist.query.get(artist_id)
   artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
+    "id": artistToEdit.id,
+    "name": artistToEdit.name,
+    #"genres": artistToEdit.genres,
+    "city": artistToEdit.city,
+    "state": artistToEdit.state,
+    "phone": artistToEdit.phone,
+    #"website": artistToEdit.website,
+    "facebook_link": artistToEdit.facebook_link,
+    #"seeking_venue": True,
+    #"seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
+    #"image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
   }
   # TODO: populate form with fields from artist with ID <artist_id>
   return render_template('forms/edit_artist.html', form=form, artist=artist)
@@ -331,20 +332,11 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
+  print("This function's job is to get the venue id's information to begin editing")
+
+  venue = Venue.query.get(venue_id)
+  print("This is what it's sending back",venue)
+  print("Just tsting that this is working how I am expecting it to",venue.city,venue.state,venue.name)
   # TODO: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
@@ -352,6 +344,38 @@ def edit_venue(venue_id):
 def edit_venue_submission(venue_id):
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
+
+
+  name = request.form['name']
+  address = request.form['address']
+  print("This is what we are getting as the address",address)
+  phone = request.form['phone']
+  genres = request.form['genres']
+  city = request.form['city']
+  state = request.form['state']
+
+  try:
+      venueToEdit = Venue.query.get(venue_id)
+      venueToEdit.name = name
+      print("Venue name is",venueToEdit.name)
+      venueToEdit.address = address
+      venueToEdit.phone = phone
+      venueToEdit.genres = genres
+      venueToEdit.city = city
+      venueToEdit.state = state
+      print("newVenue", venueToEdit)
+      db.session.add(venueToEdit)
+      db.session.commit()
+      flash('Venue ' + request.form['name'] + ' was successfully listed!')
+
+  except:
+      print("something went wrong")
+      db.session.rollback()
+      flash('An error occured! Venue ' + request.form['name'] + ' could not be listed')
+  finally:
+      db.session.close()
+
+
   return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
